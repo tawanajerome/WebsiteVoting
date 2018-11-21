811466054589b6ae908acc106535ce3257c7f450
tawanajerome <tawanajerome@mail.usf.edu>
2018-11-21 10:58:43 -0500

    added CSV code to populate login DB but not working Also made a second home page to start by uploding a csv of usernames, then being redirected to the main home login

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require 'sinatra/reloader' if development?
 require './login'
 require './voting'
 require 'bcrypt'
-
+require 'csv'
 
 configure do
   enable :sessions
@@ -12,14 +12,28 @@ get '/' do
   redirect to('home.html')
 end
 
-get '/login' do
+
+post '/file' do    #code to read the CSV and update the DB table not working
+  filename = params[:fileupload]
+ # return "#{filename.class}"
+  csv_text = File.read(filename)
+  csv = CSV.parse(csv_text, :headers => true)
+  csv.each do |row|
+    Login.create!(row.to_hash)
+  end
+  #hash all the passwords first then redirect
+  redirect to('homeLOGIN.html')
+end
+
+
+get '/login' do   #redirects the user based on their role if they've entered in the correct password
   username = params[:username]
   password = params[:password]
   @user = Login.get(username)
   session[:username] = username
   session[:password] = BCrypt::Password.new(@user.password)
   if session[:password] == password
-    if @user.role == 'student'
+    if @user.role == 'student'         #if the role is a student redirect the page
       redirect to('student.html')
     else if @user.role == 'instructor'
            redirect to('instructor.html')
