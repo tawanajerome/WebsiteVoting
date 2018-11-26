ca8f1a5b8922ee5ce3c4e972db678b7670e7649c
tawanajerome <tawanajerome@mail.usf.edu>
2018-11-26 16:06:54 -0500

    file upload csv portion to database

--- main.rb
+++ main.rb
@@ -18,10 +18,17 @@ post '/file' do    #code to read the CSV and update the DB table not working
   if file                                 #if the file has contents, erase the lagging characters and split on ,
     file = file.read.gsub(/\r\n/, ',')
     fileContent = file.split(',')
+    fileContent = fileContent.slice(4..(fileContent.length - 1))
+    puts "#{fileContent}"
     i = 0
-    while i + 4 < fileContent.length            #for every 4th element add in the username... etc. to the database
-      if i % 4 == 0 && fileContent[i] != 'username'
-        Login.new username: fileContent[i], name: fileContent[i+1], password: BCrypt::Password.create(fileContent[i+2]), role: fileContent[i+3]
+    while i + 4 < fileContent.length #for every 4th element add in the username... etc. to the database
+      if i % 4 == 0
+        if Login.get(fileContent[i]) #if the record does not exist in the table already
+          break
+        else
+          login = Login.new username: fileContent[i], name: fileContent[i+1], password: BCrypt::Password.create(fileContent[i+2]), role: fileContent[i+3]
+          login.save
+        end
       end
     end
   end
