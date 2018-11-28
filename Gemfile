1477439f2914d298927a9dd7f2ffad67c8460267
Rachel Klesius <rachelklesius@gmail.com>
2018-11-28 10:23:00 -0500

    Added Tawana's code and gemfile

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+gem "sinatra"
+gem "sinatra-contrib"
+gem "bcrypt"
+gem "dm-core"
+gem "dm-migrations"
+gem "data_mapper"
--- main.rb
+++ main.rb
@@ -8,21 +8,41 @@ require 'csv'
 configure do
   enable :sessions
 end
+
 get '/' do
-  redirect to('home.html')
+  session.clear
+  redirect to('/upload')
 end
 
+get '/upload' do
+  File.read('home.html')
+end
 
 post '/file' do    #code to read the CSV and update the DB table not working
-  filename = params[:fileupload]
- # return "#{filename.class}"
-  csv_text = File.read(filename)
-  csv = CSV.parse(csv_text, :headers => true)
-  csv.each do |row|
-    Login.create!(row.to_hash)
+  file = params[:file][:tempfile]
+  if file                                 #if the file has contents, erase the lagging characters and split on ,
+    file = file.read.gsub(/\r\n/, ',')
+    fileContent = file.split(',')
+    fileContent = fileContent.slice(4..(fileContent.length - 1))
+    puts "#{fileContent}"
+    i = 0
+    while i < fileContent.length #for every 4th element add in the username... etc. to the database
+      if i % 4 == 0
+        if Login.get(fileContent[i]) #if the record does not exist in the table already
+          break
+        else
+          login = Login.new username: fileContent[i], name: fileContent[i+1], password: BCrypt::Password.create(fileContent[i+2]), role: fileContent[i+3]
+          login.save
+        end
+      end
+      i += 1
+    end
   end
-  #hash all the passwords first then redirect
-  redirect to('homeLOGIN.html')
+  redirect to('/home')       #redirect to the actual login page
+end
+
+get '/home' do
+  File.read('homeLOGIN.html')
 end
 
 
@@ -30,18 +50,39 @@ get '/login' do   #redirects the user based on their role if they've entered in 
   username = params[:username]
   password = params[:password]
   @user = Login.get(username)
-  session[:username] = username
-  session[:password] = BCrypt::Password.new(@user.password)
-  if session[:password] == password
-    if @user.role == 'student'         #if the role is a student redirect the page
-      redirect to('student.html')
-    else if @user.role == 'instructor'
-           redirect to('instructor.html')
-         end
+  if @user
+    session[:role] = @user.role
+    session[:username] = username
+    session[:password] = BCrypt::Password.new(@user.password)
+    if session[:password] == password
+      if @user.role == 'student'         #if the role is a student redirect the page
+        redirect to('/student')
+      else if @user.role == 'instructor'    #if the role is a teacher redirect the page
+           redirect to('/instructor')
+          end
+      end
+    else
+      session.clear
+      redirect to('/incorrect')
     end
-  else
-    session.clear
-    redirect to('/incorrectlogin.html')
   end
 end
 
+get '/incorrect' do
+  File.read('incorrectlogin.html')
+end
+
+get '/student' do
+  halt(401, 'Not Authorized') unless session[:role] == 'student'
+  File.read('student.html')
+end
+
+get '/instructor' do
+  halt(401, 'Not Authorized') unless session[:role] == 'instructor'
+  File.read('instructor.html')
+end
+
+get '/logout' do
+  session.clear
+  redirect to('/home')
+end
